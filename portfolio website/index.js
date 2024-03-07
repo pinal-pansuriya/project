eda801a6cd69e10913df6b8149f7f0f01c4f9706
pinal-pansuriya <pinal24vp@gmail.com>
2024-03-07 18:02:07 +0530

    changes in portfolio

--- portfolio website/index.js
+++ portfolio website/index.js
@@ -1,18 +1,19 @@
 let submit = document.getElementById('submit')
+let bar = document.getElementById('bar')
 
 function downloadBtn() {
     alert("cv downloaded")
     window.open('resume.html')
 }
 
-submit.addEventListener('click', function () {
-    let name = document.getElementById('name')
-    let email = document.getElementById('email')
-    let msg = document.getElementById('msg')
-
-    if (name.value == "" || email.value == "" || msg.value == "") {
-        alert("Please Fill Detail")
-    } else {
-        alert("Thanks For Conecting")
+let ul = document.querySelector('ul')
+// bar.addEventlistnner('click',function(){
+    function barr(){
+    ul.classList.toggle('showData')
+    if(ul.classList == 'showData'){
+        bar.className = 'bx bx-x'
+    }else{
+        bar.className = 'bx bx-menu'
     }
-})
+}
+
